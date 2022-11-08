a22e7597e29a45aa5586f19fec05c364af8d684f
Ralf <balz@balz-software.de>
2022-11-08 19:32:53 +0100

    Add Retofit

--- app/src/main/java/ch/plebsapps/mealsapp/ui/meals/MainActivity.kt
+++ app/src/main/java/ch/plebsapps/mealsapp/ui/meals/MainActivity.kt
@@ -3,11 +3,18 @@ package ch.plebsapps.mealsapp.ui.meals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-//import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import ch.plebsapps.mealsapp.ui.theme.MealsAppTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import ch.plebsapps.mealsapp.model.response.MealsResponse
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +30,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MealsCategoriesScreen() {
-    //val viewModel = viewModel()
-    Text(text = "Hello Compose")
+    val viewModel: MealsCategoriesViewModel = viewModel()
+    val remeberedMeals: MutableState<List<MealsResponse>> = remember { mutableStateOf(emptyList<MealsResponse>()) }
+
+    viewModel.getMeals{ response ->
+        val mealsFromTheApi = response?.categories
+        remeberedMeals.value = mealsFromTheApi.orEmpty()
+    }
+    LazyColumn {
+        items(remeberedMeals.value) { meal ->
+            Text(text = meal.name)
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/ch/plebsapps/mealsapp/ui/meals/MealsCategoriesViewModel.kt
+++ app/src/main/java/ch/plebsapps/mealsapp/ui/meals/MealsCategoriesViewModel.kt
@@ -2,6 +2,13 @@ package ch.plebsapps.mealsapp.ui.meals
 
 import androidx.lifecycle.ViewModel
 import ch.plebsapps.mealsapp.model.MealsRepository
+import ch.plebsapps.mealsapp.model.response.MealsCategoriesResponse
+import ch.plebsapps.mealsapp.model.response.MealsResponse
 
 class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRepository() ) : ViewModel() {
+    fun getMeals(successCallback:(response: MealsCategoriesResponse?) -> Unit) {
+        repository.getMeals(){ response ->
+            successCallback(response)
+        }
+    }
 }
